e7233f9d0681ad2de4e5c1229456fb4989af0d74
zyprosoft <1003081775@qq.com>
2021-01-29 17:06:36 +0800

    fix email service
    fix email service

--- src/Service/EmailService.php
+++ src/Service/EmailService.php
@@ -12,9 +12,6 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-// Load Composer's autoloader
-require 'vendor/autoload.php';
-
 class EmailService
 {
     protected function mailer()
